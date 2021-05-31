957c219c848f404a0e90f5ba9be417106ec0212f
dca-davi <dca_davi@hotmail.com>
2021-05-31 17:18:55 -0400

    array

---
+++ array/array.js
@@ -0,0 +1,11 @@
+let aprovados = new Array('Bia', 'Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
