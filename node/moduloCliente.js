7c52229002ce3246a4cccf085448c227dac70432
dca-davi <dca_davi@hotmail.com>
2021-06-05 21:27:56 -0400

    Filter,Modelo

---
+++ node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA')
+const modulob = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(modulob.bomDia)
+console.log(modulob.boanoite())
+console.log(modulob)
